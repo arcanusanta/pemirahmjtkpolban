91d337f703d055e9f515bfb17d9214edb72879c6
princegarll <official@princegarll.xyz>
2023-11-18 06:36:21 +0300

    FEAT : Grade - Read, Create, & Delete

---
+++ app/Http/Controllers/GradeController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\GradeRequest;
+use App\Models\Grade;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+use Yajra\DataTables\DataTables;
+
+class GradeController extends Controller
+{
+    public function index()
+    {
+        if (request()->ajax()) {
+            $Data = Grade::latest()->get();
+
+            return DataTables::of($Data)->addIndexColumn()->addColumn('action', 'master.voters.grade.action')->rawColumns(['action'])->make(true);
+        }
+
+        $Title = "Kelas";
+
+        return view('master.voters.grade.index', compact('Title'));
+    }
+
+    public function create()
+    {
+        $Title = "Tambah Kelas";
+
+        return view('master.voters.grade.create', compact('Title'));
+    }
+
+    public function store(GradeRequest $Request)
+    {
+        try {
+            Grade::create([
+                'name' => $Request->name,
+            ]);
+
+            Alert::success('Selamat', 'Anda telah berhasil menambahkan data');
+            return redirect()->route('voters.grade.index');
+        } catch (\Exception $Excep) {
+            Alert::error('Error', $Excep->getMessage());
+            return redirect()->route('voters.grade.index');
+        }
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        try {
+            Grade::where('id', $id)->delete();
+
+            Alert::success('Selamat', 'Anda telah berhasil menghapus data');
+            return redirect()->route('voters.grade.index');
+        } catch (\Exception $Excep) {
+            Alert::error('Error', $Excep->getMessage());
+            return redirect()->route('voters.grade.index');
+        }
+    }
+}
---
+++ app/Models/StudyProgram.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+
+class StudyProgram extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'id' => 'string',
+    ];
+
+    public $incrementing = false;
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function($obj) {
+            $obj->id = RamseyUuid::uuid4()->toString();
+        });
+    }
+}
